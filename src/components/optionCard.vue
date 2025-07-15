b9f99a6fb189575368470e986d491cabbd381dd3
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-15 08:28:26 -0300

    creating optionCard component

---
+++ src/components/optionCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="option-card">
+        <p>A</p>
+    </div>
+</template>
+
+<style scoped>
+.option-card {
+    background-color: oklch(80.9% 0.105 251.813);
+    color: #fff;
+    padding: 15px 25px;
+    border-radius: 10px;
+    width: 120px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 1.2em;
+    font-weight: bold;
+    cursor: pointer;
+    transition: transform 0.2s ease;
+}
+
+.option-card:hover {
+    transform: scale(1.05);
+}
+</style>
